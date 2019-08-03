5ecabb2e9a399c3baee3a6d157ed55570cb25ddf
natsukium <tm.tomatoes@gmail.com>
2019-08-03 20:40:12 +0900

    Implement '>' '<' '+' '-' '.' ','
    '[' and ']' have not be implemented yet.

--- src/Fsy/Fsy.fs
+++ src/Fsy/Fsy.fs
@@ -1,6 +1,27 @@
 module Fsy
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+let interpet code =
+    let memory = Array.zeroCreate 30000
+    let mutable ptr = 0
+
+    let rec read code =
+        match code with
+        | c :: code ->
+            match c with
+            | '>' -> ptr <- ptr + 1
+            | '<' -> ptr <- ptr - 1
+            | '+' -> memory.[ptr] <- memory.[ptr] + 1
+            | '-' -> memory.[ptr] <- memory.[ptr] - 1
+            | '.' ->
+                memory.[ptr]
+                |> char
+                |> printf "%O"
+            | ',' -> memory.[ptr] <- stdin.ReadLine() |> int
+            | _ -> ptr <- ptr
+            read code
+        | _ -> ptr <- ptr
+    code
+    |> Seq.toList
+    |> read
+
+stdin.ReadLine() |> interpet
